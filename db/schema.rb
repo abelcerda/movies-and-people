1f41664786abd422f9f3633afd421ab45ccb7d2d
Abel Antonio Cerda <abelantoniocerda@gmail.com>
2020-11-01 14:06:28 -0300

    Create Credit model, model test, and migration. Update schema with new Credit table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_164833) do
+ActiveRecord::Schema.define(version: 2020_11_01_170230) do
+
+  create_table "credits", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
