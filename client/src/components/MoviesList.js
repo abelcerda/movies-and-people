9c87faaa12866b8879ce8f5c362921d6de60d2c1
Abel Antonio Cerda <abelantoniocerda@gmail.com>
2020-11-02 20:18:48 -0300

    Add UI elements and styling to display the movies data.

--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styles from "./shared-styles.module.css";
+
+function mapPersonName(people) {
+  return (
+    people
+      .map(({ first_name, last_name }) => `${first_name} ${last_name}`)
+      .join(", ") || "Unknown"
+  );
+}
 
 const MoviesList = (props) => {
   useEffect(() => {
@@ -9,11 +18,23 @@ const MoviesList = (props) => {
   const [movies, setMovies] = useState([]);
 
   return (
-    <div>
-      <div className="movies-list">
+    <div className={styles.container}>
+      <div className={styles.content}>
         {movies.map((movie, index) => (
-          <div key={index}>
-            {movie.title} | {movie.release_year}
+          <div key={index} className={styles.item}>
+            <span className={styles.itemTitle}>{movie.title}</span>
+            <div className={styles.itemContent}>
+              <span className={styles.itemSubtitle}>{movie.release_year}</span>
+              <span className={styles.itemSubtitle}>
+                Directed by <b>{mapPersonName(movie.directors)}</b>
+              </span>
+              <span className={styles.itemSubtitle}>
+                Produced by <b>{mapPersonName(movie.producers)}</b>
+              </span>
+              <span className={styles.itemSubtitle}>
+                Starred by <b>{mapPersonName(movie.casting)}</b>
+              </span>
+            </div>
           </div>
         ))}
       </div>
