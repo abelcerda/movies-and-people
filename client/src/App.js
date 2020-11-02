ab395d3aebbededbec5c13280d6797a1fd129760
Abel Antonio Cerda <abelantoniocerda@gmail.com>
2020-11-02 20:26:24 -0300

    Move the state management to the root component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import MoviesList from "./components/MoviesList";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/v1/movies.json").then((res) => setMovies(res.data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movies and People</h1>
       </header>
-      <MoviesList />
+      <MoviesList movies={movies} />
     </div>
   );
 }
--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import styles from "./shared-styles.module.css";
 
 function mapPersonName(people) {
@@ -10,35 +9,27 @@ function mapPersonName(people) {
   );
 }
 
-const MoviesList = (props) => {
-  useEffect(() => {
-    axios.get("/api/v1/movies.json").then((res) => setMovies(res.data));
-  }, []);
-
-  const [movies, setMovies] = useState([]);
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        {movies.map((movie, index) => (
-          <div key={index} className={styles.item}>
-            <span className={styles.itemTitle}>{movie.title}</span>
-            <div className={styles.itemContent}>
-              <span className={styles.itemSubtitle}>{movie.release_year}</span>
-              <span className={styles.itemSubtitle}>
-                Directed by <b>{mapPersonName(movie.directors)}</b>
-              </span>
-              <span className={styles.itemSubtitle}>
-                Produced by <b>{mapPersonName(movie.producers)}</b>
-              </span>
-              <span className={styles.itemSubtitle}>
-                Starred by <b>{mapPersonName(movie.casting)}</b>
-              </span>
-            </div>
+const MoviesList = ({ movies }) => (
+  <div className={styles.container}>
+    <div className={styles.content}>
+      {movies.map((movie, index) => (
+        <div key={index} className={styles.item}>
+          <span className={styles.itemTitle}>{movie.title}</span>
+          <div className={styles.itemContent}>
+            <span className={styles.itemSubtitle}>{movie.release_year}</span>
+            <span className={styles.itemSubtitle}>
+              Directed by <b>{mapPersonName(movie.directors)}</b>
+            </span>
+            <span className={styles.itemSubtitle}>
+              Produced by <b>{mapPersonName(movie.producers)}</b>
+            </span>
+            <span className={styles.itemSubtitle}>
+              Starred by <b>{mapPersonName(movie.casting)}</b>
+            </span>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 export default MoviesList;
